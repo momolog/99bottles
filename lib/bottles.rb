44f59e8ed4c806c256c139e4622edcbf42ebdcf0
Alexander Presber <post@momolog.info>
2019-07-15 00:03:21 +0200

    minor styling

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,32 +4,33 @@ class Bottles
   end
 
   def verses nr1, nr2
-    nr1.downto(nr2).map do |nr|
-      verse(nr)
-    end.join("\n")
+    nr1.downto(nr2).map{|nr| verse(nr) }.join("\n")
   end
 
   def verse nr
     <<~V
       #{bottle_words(nr).capitalize} of beer on the wall, #{bottle_words(nr)} of beer.
-      #{action_words(nr)}
+      #{action(nr)}
     V
   end
 
   def bottle_words nr
     case nr
-      when 0 then  "no more bottles"
+      when 0 then  'no more bottles'
       else         "#{nr} bottle#{nr>1 ? 's' : ''}"
     end
   end
 
   def take_word nr
-    nr>1 ? "one" : "it"
+    case nr
+      when 1 then 'it'
+      else        'one'
+    end
   end
 
-  def action_words(nr)
+  def action(nr)
     case nr
-      when 0 then "Go to the store and buy some more, 99 bottles of beer on the wall."
+      when 0 then 'Go to the store and buy some more, 99 bottles of beer on the wall.'
       else        "Take #{take_word(nr)} down and pass it around, #{bottle_words(nr-1)} of beer on the wall."
     end
   end
